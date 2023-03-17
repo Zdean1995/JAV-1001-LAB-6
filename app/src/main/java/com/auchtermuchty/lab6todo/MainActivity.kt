2269e5c9435b609d1e98e277d3cf7225fe8bc12b
Zachary <zdean1995@gmail.com>
2023-03-16 22:32:47 -0400

    Finished project.  Added comments and finishing touches.

--- app/src/main/java/com/auchtermuchty/lab6todo/MainActivity.kt
+++ app/src/main/java/com/auchtermuchty/lab6todo/MainActivity.kt
@@ -2,21 +2,22 @@ package com.auchtermuchty.lab6todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.AdapterView.OnItemClickListener
-import androidx.recyclerview.widget.RecyclerView
 import com.auchtermuchty.lab6todo.adapter.TodoAdapter
 import com.auchtermuchty.lab6todo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.recTodo.adapter = TodoAdapter(this)
+        binding.recTodo.adapter = TodoAdapter()
 
+        //sets the on click listener to add the edit view text to the recycler view
+        //references: https://stackoverflow.com/questions/27845069/add-a-new-item-to-recyclerview-programmatically
         binding.btnAdd.setOnClickListener {
             (binding.recTodo.adapter as TodoAdapter).addTodoItem(binding.edtxtAdd.text.toString())
             (binding.recTodo.adapter as TodoAdapter).notifyItemInserted((binding.recTodo.adapter as TodoAdapter).itemCount -1)
--- app/src/main/java/com/auchtermuchty/lab6todo/adapter/TodoAdapter.kt
+++ app/src/main/java/com/auchtermuchty/lab6todo/adapter/TodoAdapter.kt
@@ -1,6 +1,5 @@
 package com.auchtermuchty.lab6todo.adapter
 
-import android.content.Context
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
@@ -9,9 +8,12 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.auchtermuchty.lab6todo.R
 
-class TodoAdapter(private val context: Context)
+//The adapter for the recycler view in the main activity
+//references: https://developer.android.com/codelabs/basic-android-kotlin-training-recyclerview-scrollable-list
+class TodoAdapter
     : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
 
+    //the list of to do Items
     private var todoStrings: MutableList<String> = mutableListOf()
 
     class TodoViewHolder (view: View) : RecyclerView.ViewHolder(view) {
@@ -30,17 +32,22 @@ class TodoAdapter(private val context: Context)
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
         holder.txtTodo.text = todoStrings[position]
+
+        //the on click listeners for the text views in the recycler views
+        //this sets the done text view visible when the user clicks on the to do items
         holder.txtTodo.setOnClickListener {
             holder.txtDone.visibility = View.VISIBLE
             holder.txtTodo.setTextColor(Color.GRAY)
         }
 
+        //clicking the done text will get rid of it again, just in case the user clicked it by accident
         holder.txtDone.setOnClickListener {
             holder.txtDone.visibility = View.GONE
             holder.txtTodo.setTextColor(Color.BLACK)
         }
     }
 
+    //The method for adding new to do items to the list of to do items
     fun addTodoItem(todoString: String) {
         todoStrings.add(todoString)
     }
